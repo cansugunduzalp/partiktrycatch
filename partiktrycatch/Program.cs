3976d52dfa3edecb8d1caaec9374ab2ff7152670
cansugunduzalp <cansugunduzalp@gmail.com>
2025-01-26 01:04:13 +0300

    Add project files.

---
+++ partiktrycatch/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Bir sayı giriniz:");
+
+        try
+        {
+            // Kullanıcının girdiği değeri alıyoruz
+            string input = Console.ReadLine();
+
+            // Kullanıcının girdiği değeri sayıya dönüştürmeye çalışıyoruz
+            int sayi = int.Parse(input);
+
+            // Sayının karesini hesaplayıp ekrana yazdırıyoruz
+            int kare = sayi * sayi;
+            Console.WriteLine($"Girdiğiniz sayının karesi: {kare}");
+        }
+        catch (FormatException)
+        {
+            // Eğer kullanıcı geçersiz bir giriş yaparsa
+            Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+        }
+    }
+}
